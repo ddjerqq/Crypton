176a2b71e9f536c32e3be419cde6a6cc58304021
Giorgi Nachkebia <gio2004n@gmail.com>
2023-08-10 02:14:26 +0400

    add CalculateSha256HexDigest to StringExtensions.cs

--- Crypton.Domain/Common/Extensions/StringExtensions.cs
+++ Crypton.Domain/Common/Extensions/StringExtensions.cs
@@ -2,20 +2,44 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Crypton.Domain.Common.Extensions;
 
-public static class StringExtensions
+/// <summary>
+/// String extensions.
+/// </summary>
+public static partial class StringExtensions
 {
     public static string ToSnakeCase(this string input)
     {
-        if (string.IsNullOrEmpty(input))
-        {
-            return input;
-        }
+        if (string.IsNullOrEmpty(input)) return input;
 
-        var startUnderscores = Regex.Match(input, @"^_+");
-        return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        var startUnderscores = Word().Match(input);
+        return startUnderscores + CamelCase().Replace(input, "$1_$2").ToLower();
     }
+
+    public static string CalculateSha256HexDigest(this string input)
+    {
+        var bytes = Encoding.UTF8.GetBytes(input);
+        var hash = SHA256.HashData(bytes);
+        var digest = BitConverter.ToString(hash);
+        return digest.Replace("-", string.Empty).ToLower();
+    }
+
+    /// <summary>
+    /// regex for words
+    /// </summary>
+    /// <returns></returns>
+    [GeneratedRegex("^_+")]
+    private static partial Regex Word();
+
+    /// <summary>
+    /// camel case word
+    /// </summary>
+    /// <returns></returns>
+    [GeneratedRegex("([a-z0-9])([A-Z])")]
+    private static partial Regex CamelCase();
 }
