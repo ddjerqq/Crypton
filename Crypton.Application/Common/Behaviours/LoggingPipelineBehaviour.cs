2e325771d315d1a2eb32d282666765089f2fa28e
Giorgi Nachkebia <gio2004n@gmail.com>
2023-08-10 02:10:44 +0400

    reformat the LoggingPipelineBehaviour.cs

--- Crypton.Application/Common/Behaviours/LoggingPipelineBehaviour.cs
+++ Crypton.Application/Common/Behaviours/LoggingPipelineBehaviour.cs
@@ -1,4 +1,8 @@
-﻿using ErrorOr;
+﻿// <copyright file="LoggingPipelineBehaviour.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using ErrorOr;
 using MediatR;
 using Microsoft.Extensions.Logging;
 
@@ -9,28 +13,32 @@ public sealed class LoggingPipelineBehaviour<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
     where TResponse : IErrorOr
 {
-    private readonly ILogger<LoggingPipelineBehaviour<TRequest, TResponse>> _logger;
+    private readonly ILogger<LoggingPipelineBehaviour<TRequest, TResponse>> logger;
 
     public LoggingPipelineBehaviour(ILogger<LoggingPipelineBehaviour<TRequest, TResponse>> logger)
     {
-        _logger = logger;
+        this.logger = logger;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
     {
-        _logger.LogInformation("started request {@RequestName} {@Request}", typeof(TRequest).Name, request);
+        this.logger.LogInformation("started request {@RequestName} {@Request}", typeof(TRequest).Name, request);
 
         var start = DateTime.UtcNow;
         var result = await next();
-        double end = (DateTime.UtcNow - start).TotalMilliseconds;
+        var end = (DateTime.UtcNow - start).TotalMilliseconds;
 
-        _logger.LogInformation("finished request {@RequestName} {@Request} in {@Duration}ms", typeof(TRequest).Name,
+        this.logger.LogInformation("finished request {@RequestName} {@Request} in {@Duration}ms", typeof(TRequest).Name,
             request, end);
 
         // if result returned an error explicitly, and didnt throw an error, we will log it
         if (result.IsError)
-            _logger.LogError("request {@RequestName} {@Request} returned errors {@Error}", typeof(TRequest).Name,
+        {
+            this.logger.LogError(
+                "request {@RequestName} {@Request} returned errors {@Error}",
+                typeof(TRequest).Name,
                 request, result.Errors);
+        }
 
         return result;
     }
