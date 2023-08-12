4495f0fa773540e6e8edba914b04121470b97598
Giorgi Nachkebia <gio2004n@gmail.com>
2023-08-12 16:03:28 +0400

    modify TransactionController.cs remove GetBlock by id and by index. add new endpoint CreateTransaction, which is validated and sent to mediatr to be handled.

--- Crypton.WebAPI/Controllers/TransactionController.cs
+++ Crypton.WebAPI/Controllers/TransactionController.cs
@@ -1,11 +1,10 @@
 ﻿using Crypton.Application.Interfaces;
 using Crypton.Application.Transactions;
-using Crypton.Domain.Common.Extensions;
 using Crypton.Domain.Entities;
+using Crypton.Infrastructure.ModelBinders;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Crypton.WebAPI.Controllers;
 
@@ -16,18 +15,15 @@ namespace Crypton.WebAPI.Controllers;
 public sealed class TransactionController : ControllerBase
 {
     private readonly IMediator mediator;
-    private readonly IAppDbContext dbContext;
     private readonly ITransactionService transactionService;
     private readonly ICurrentUserAccessor currentUserAccessor;
 
     public TransactionController(
         IMediator mediator,
-        IAppDbContext dbContext,
         ITransactionService transactionService,
         ICurrentUserAccessor currentUserAccessor)
     {
         this.mediator = mediator;
-        this.dbContext = dbContext;
         this.transactionService = transactionService;
         this.currentUserAccessor = currentUserAccessor;
     }
@@ -61,46 +57,15 @@ public sealed class TransactionController : ControllerBase
         return this.Ok(orderedTransactions);
     }
 
-    [HttpGet("{id:guid}")]
-    public IActionResult GetBlockById(Guid id)
-    {
-        var block = this.transactionService.Transactions
-            .FirstOrDefault(x => x.Id == id);
-
-        if (block is null)
-            return this.NotFound();
-
-        return this.Ok(block);
-    }
-
-    [HttpGet("{id:long}")]
-    public IActionResult GetBlockByIndex(long id)
-    {
-        var block = this.transactionService.Transactions
-            .FirstOrDefault(x => x.Index == id);
-
-        if (block is null)
-            return this.NotFound();
-
-        return this.Ok(block);
-    }
-
     [HttpPost("daily")]
     public async Task<IActionResult> CollectDaily(CancellationToken ct = default)
     {
         var currentUser = await this.currentUserAccessor.GetCurrentUserAsync(ct);
 
-        var command = new CreateTransactionCommand
-        {
-            SenderId = GuidExtensions.ZeroGuidValue,
-            Sender = Domain.Entities.User.SystemUser(),
-            ReceiverId = currentUser!.Id,
-            Receiver = currentUser,
-            TransactionType = TransactionType.BalanceTransaction,
-            Amount = 100,
-            ItemId = null,
-            Item = null,
-        };
+        if (currentUser is null)
+            return this.Unauthorized();
+
+        var command = new CreateTransactionCommand(null, currentUser, 100);
 
         var result = await this.mediator.Send(command, ct);
         if (result.IsError)
@@ -109,38 +74,14 @@ public sealed class TransactionController : ControllerBase
         return this.Ok();
     }
 
-    // TODO this should be a validatable command
-    [HttpPost("send_payment/{receiverId:guid}")]
+    [HttpPost("create")]
     public async Task<IActionResult> CreateTransaction(
-        Guid receiverId,
-        [FromQuery] decimal amount,
+        [FromBody] [ModelBinder(BinderType = typeof(CreateTransactionCommandModelBinder))]
+        CreateTransactionCommand command,
         CancellationToken ct = default)
     {
-        var currentUser = await this.currentUserAccessor.GetCurrentUserAsync(ct);
-        var receiver = await this.dbContext
-            .Users.AsNoTracking()
-            .FirstOrDefaultAsync(x => x.Id == receiverId.ToString(), ct);
-
-        // these will need to go to MediatR
-        if (currentUser is null || receiver is null)
-            return this.BadRequest("Invalid user id");
-
-        if (amount <= 0 || amount > currentUser.Balance)
-            return this.BadRequest("Invalid amount");
-
-        var command = new CreateTransactionCommand
-        {
-            SenderId = currentUser.Id,
-            Sender = currentUser,
-            ReceiverId = receiver.Id,
-            Receiver = receiver,
-            TransactionType = TransactionType.BalanceTransaction,
-            Amount = amount,
-            ItemId = null,
-            Item = null,
-        };
-
         var result = await this.mediator.Send(command, ct);
+
         if (result.IsError)
             return this.BadRequest(result.Errors);
 
