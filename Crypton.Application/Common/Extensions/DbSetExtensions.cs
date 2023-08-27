e63dc8b6a1f19a06982bd56f4d73d082fdd23ff3
Giorgi <gio2004n@gmail.com>
2023-08-27 04:22:25 +0400

    add DbSetExtensions.cs

---
+++ Crypton.Application/Common/Extensions/DbSetExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Crypton.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace Crypton.Application.Common.Extensions;
+
+public static class DbSetExtensions
+{
+    public static EntityEntry<TEntity>? TryUpdateIfNotNull<TEntity>(this DbSet<TEntity> dbSet, TEntity? entity)
+        where TEntity : class
+    {
+        try
+        {
+            if (entity is not null)
+            {
+                return dbSet.Update(entity);
+            }
+        }
+        catch (InvalidOperationException ex)
+        {
+            // the entity is already being tracked by the DbContext
+            // common causes: the entity is the current user.
+            var logger = dbSet.GetService<ILogger<IAppDbContext>>();
+            logger.LogDebug(ex, "The entity is already being tracked by the DbContext");
+            logger.LogInformation("The entity is already being tracked by the DbContext");
+        }
+
+        return null;
+    }
+}
